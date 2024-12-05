3a92b11894166b570aad128f64ff9f4a5a93d2a4
William Woodard <williamwoodard98@gmail.com>
2024-12-05 12:50:53 -0600

    adding models

---
+++ models/ServiceTicket.cs
@@ -0,0 +1,11 @@
+namespace HoneyRaesAPI.Models;
+
+public class ServiceTicket 
+{
+    public int Id { get; set; }
+    public string CustomerId { get; set; }
+    public string EmployeeId { get; set; }
+    public string Description { get; set; }
+    public Boolean Emergency { get; set; }
+    public DateTime DateCompleted { get; set; }
+}
